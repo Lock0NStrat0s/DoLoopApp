d9b87666c3b8a20b6a6de378985c7d3a2c685e63
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-10 23:49:14 -0600

    Add project files.

---
+++ DoLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿
+bool isValidAge;
+int age;
+
+do
+{
+    Console.Write("What is your age: ");
+    string ageText = Console.ReadLine();
+
+    isValidAge = int.TryParse(ageText, out age);
+
+    if (!isValidAge)
+    {
+        Console.WriteLine("That was an invalid age.");
+    }
+} while (!isValidAge);
+
+Console.WriteLine($"You age is {age}.");
